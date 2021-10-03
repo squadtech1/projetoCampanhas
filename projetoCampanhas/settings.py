5e55c1f08e66824c9830161085514701c4805a0f
vitorcvargas <vitorcvargas@gmail.com>
2021-10-03 09:13:34 -0300

    criacao do component de autenticacao e criacao do superuser

--- projetoCampanhas/settings.py
+++ projetoCampanhas/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'projetoCampanhas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'auth/template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
