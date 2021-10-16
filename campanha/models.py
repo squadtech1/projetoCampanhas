703b5a21f852921fe69e674d34ed0b905cde104e
Marco <you@example.com>
2021-10-16 15:09:41 -0300

    Classe DadosCadastrais criada

--- campanha/models.py
+++ campanha/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class DadosCadastrais(models.Model):
+
+	nome = models.Charfield(
+		max_length=255,
+		null=False,
+		blank=False
+	)
+	
+	cpf = models.CharField(
+ 		max_length=14,
+ 		null=False,
+ 		blank=False
+ 	)
+ 
+	
+ 	cnpj = models.CharField(
+ 		max_length=18,
+ 		null=False,
+		blank=False
+	)
+		
+	email = models.Charfield(
+		max_length=255,
+		null=False,
+		blank=False
+	)
+	
+	telefone = models.CharField(
+		max_length= 100,
+		null=False,
+		blank=False
+    )
