f6952b633677f3a303a576de2cc610490c120893
Marco <you@example.com>
2021-10-25 15:41:42 -0300

    models

--- campanha/models.py
+++ campanha/models.py
@@ -1,34 +1,42 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
+from django.db.models.manager import ManagerDescriptor
 
 class DadosCadastrais(models.Model):
 
-	nome = models.CharField(
-		max_length=255,
-		null=False,
-		blank=False,
-	)
+	nome = models.CharField(max_length=255)
+
+	cpf = models.CharField(max_length=14)	
+ 	
+	cnpj = models.CharField(max_length=18)		
 	
-	cpf = models.CharField(
- 		max_length=14,
- 		null=False,
- 		blank=False
- 	)
- 
+	email = models.EmailField(max_length=100)	
 	
- 	cnpj = models.CharField(
- 		max_length=18,
- 		null=False,
-		blank=False
-	)
-		
-	email = models.CharField(
-		max_length=255,
-		null=False,
-		blank=False,
-	)
+	telefone = models.CharField(max_length= 100)
+
+class Doador(models.Model):
+
+	IdDadosCadastrais = ForeignKey(DadosCadastrais, on_delete=models.CASCADE, verbose_name= 'Os Dados Cadastrais')
 	
-	telefone = models.CharField(
-		max_length= 100,
-		null=False,
-		blank=False
-    )
+	DadosCadastrais = models.OneToOneField(DadosCadastrais, on_delete=models.CASCADE,verbose_name= 'Os Dados Cadastrais')
+
+	doacao = models.CharField(max_length= 100)
+
+	quantidade = models.CharField(max_length= 10)
+
+class EntidadeOrganizadora(models.Model):
+
+	idDadosCadastrais = ForeignKey(DadosCadastrais, on_delete=models.CASCADE, verbose_name= 'Os Dados Cadastrais')
+
+	DadosCadastrais = models.OneToOneField(DadosCadastrais, on_delete=models.CASCADE, verbose_name= 'Os Dados Cadastrais')
+
+	Responsavel = models.CharField(max_length= 100)
+
+class Campanha(models.Model):
+
+	doadores = models.ManyToManyField(Doador)
+
+	nome = models.CharField(max_length=255)
+
+	beneficiados = models.CharField(max_length=255)
+	
