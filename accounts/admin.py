a8570b36d9f041597610fc0f3fe5f5f1a3aaaee3
Vitor Cardoso <vitor.cardoso@br.ey.com>
2021-11-07 07:08:45 -0300

    criacao do model Donation Item

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from django.contrib import admin
 from django.contrib.auth import admin as authAdmin
-from django.db import models
-
-from campanha.models import Campanha
 from .forms import UserCreationForm, UserChangeForm
 from .models import User
-
-admin.site.register(Campanha)
 
 @admin.register(User)
 class UserAdmin(authAdmin.UserAdmin):
