f7d10bef0a97419bbcde2d3bbfe8883156919cec
Thiago S. de Angele <deangele@outlook.com>
2021-11-28 23:58:32 -0300

    Cadastro e Login atualizados, menu muda o Faça Parte para logout se estiver logado

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,14 +17,8 @@ class UserCreationForm(forms.UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['role'].widget.attrs.update({'class': 'roleField'})
-        self.fields['role'].label = 'Qual papel você quer ter?'
         self.fields['entity_type'].widget.attrs.update({'class': 'entityTypeField'})
-        self.fields['entity_type'].label = 'Você é uma pessoa física ou jurídica?'
         self.fields['cpf_cnpj'].widget.attrs.update({'class': 'cpfCnpjField'})
-        self.fields['cpf_cnpj'].label = 'Informe o seu CPF ou CNPJ'
         self.fields['email'].widget.attrs.update({'class': 'emailField'})
-        self.fields['email'].label = 'Endereço de email'
         self.fields['street'].widget.attrs.update({'class': 'streetField'})
-        self.fields['street'].label = 'Endereço'
         self.fields['state'].widget.attrs.update({'class': 'stateField'})
-        self.fields['state'].label = 'Estado'
