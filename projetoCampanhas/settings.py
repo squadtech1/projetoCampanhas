95332963c05d2813a11c20cda6b47a62a506a2d8
Vitor Cardoso <vitor.cardoso@br.ey.com>
2021-12-08 18:55:54 -0300

    config heroku

--- projetoCampanhas/settings.py
+++ projetoCampanhas/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from django.urls import reverse_lazy
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,3 +149,5 @@ LOGOUT_REDIRECT_URL = reverse_lazy('painel')
 
 
 AUTH_USER_MODEL = "accounts.User"
+
+django_heroku.settings(locals())
